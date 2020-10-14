1a01918c1b56aa41fdd1c37b205a5e8f810d64f6
shirajun <shirajun3891@icloud.com>
2020-10-14 20:44:39 +0900

    ユーザー登録機能実装　修正２

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,8 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.initials(number: 2) }
     email { Faker::Internet.free_email }
-    # password = Faker::Internet.password(min_length: 6)
-    password { "abc123" }
-    password_confirmation { "abc123" }
+    password { 'abc123' }
+    password_confirmation { 'abc123' }
     transient do
       person { Gimei.name }
     end
