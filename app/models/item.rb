d2d8ae3504837d8fb6b07895ad179e0c35c47887
shirajun <shirajun3891@icloud.com>
2020-10-15 13:59:04 +0900

    関連Gem導入〜バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :condition, :shipping_format, :prefecture, :day
+  has_one_attached :image
+
+  validates :image, :name, :text, :price, :category, :condition,:shipping_format, :prefecture, :day, presence: true
+  
+  with_options numericality: { other_than: 1 } do
+    validates :category_id, :condition_id, :shipping_format_id, :prefecture_id :day_id,
+  end
+
+  belongs_to :user
 end
