f877bfe625bd76af24441ec576d14e8305ff5143
shirajun <shirajun3891@icloud.com>
2020-10-16 15:43:26 +0900

    販売手数料・利益の実装完了

---
+++ app/javascript/price.js
@@ -0,0 +1,12 @@
+function price (){
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue / 10);
+    const addProfitDom = document.getElementById("profit");
+    addProfitDom.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);
+  });
+}
+
+window.addEventListener('load', price);
