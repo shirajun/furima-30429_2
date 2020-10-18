51d96ed0e5f1bd67b2ca2c486f72932cafa35ba4
shirajun <shirajun3891@icloud.com>
2020-10-18 17:26:16 +0900

    修正しました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -20,16 +21,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path unless current_user.id = @item.user_id
+    unless current_user.id = @item.user_id
+      redirect_to root_path 
+    end
   end
 
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.valid?
       redirect_to item_path
@@ -55,5 +55,9 @@ class ItemsController < ApplicationController
           .merge(
             user_id: current_user.id
           )
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
